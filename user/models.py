bbd768276586712d59866447dd4c8b8e5e5f2c29
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-08-08 16:52:40 +0430

    Customization of django user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from base.models import BaseModel
+
+
+class User(AbstractUser):
+    """
+    Custom User Model
+    """
+    phone_n = models.CharField(max_length=20, verbose_name=_('Phone'))
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+    def __str__(self):
+        return self.username
