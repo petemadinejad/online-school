6c5e93adc2e43a7dd6dfcde5d1ce359bbfe43743
parisaetemadinejad <parisa.etemadinejad@gmail.com>
2022-08-08 17:13:16 +0430

    change school model

--- schools/models.py
+++ schools/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 from base.models import BaseModel
 
 
 class School(BaseModel):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255, blank=True)
-    zipcode = models.CharField(max_length=255, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
     phone = models.CharField(max_length=255)
     website = models.CharField(max_length=255, blank=True)
-    school_type = models.CharField(max_length=255)
-    head_master = models.OneToOneField("User", on_delete=models.CASCADE, related_name="school_head_master",)
+    school_type = models.ForeignKey('SchoolType', on_delete=models.DO_NOTHING)
+    head_master = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="school_head_master", )
 
     def __str__(self):
         return self.name
@@ -18,3 +18,7 @@ class School(BaseModel):
     class Meta:
         verbose_name = 'school'
         verbose_name_plural = 'schools'
+
+
+class SchoolType(models.TextChoices):
+    pass
