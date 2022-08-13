b6fa18dd4f206607fb61930ec0dab5e071f3cf37
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-08-13 23:47:45 +0430

    Adding serializers module for user app

---
+++ user/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from user.models import User
+
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        read_only_fields = ["id", "password"]
