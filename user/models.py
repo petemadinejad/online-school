b92c77953659ba1cbdfb1f7031160349b4c2116e
petemadinejad <parisaetemadinejad.job@gmail.com>
2022-08-13 15:05:55 +0430

    add avatar folder

--- user/models.py
+++ user/models.py
@@ -1,15 +1,20 @@
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    @staticmethod
+    def get_upload_path(instance, filename):
+        return os.path.join("user", "%d" % instance.id, filename)
     """
     Custom User Model
     """
     REQUIRED_FIELDS = ['phone_number']
     phone_number = models.CharField(max_length=20, verbose_name=_('Phone'))
-    image = models.ImageField(verbose_name=_('Image'), upload_to='user/{}/'.format(str(AbstractUser.username)), blank=True)
+    image = models.ImageField(verbose_name=_('Image'), upload_to=get_upload_path, blank=True)
 
     class Meta:
         db_table = 'user'
@@ -18,3 +23,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
